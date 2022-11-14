7342188222d810cbba7b01e0b48c32d2f0db8297
Iskander <kubeev2001@gmail.com>
2022-11-14 19:39:03 +0600

    Form added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import Maska from "maska";
 
 
 const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+    .use(router)
+    .use(Maska)
+    .mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+    Card is added
   </div>
 </template>
 
